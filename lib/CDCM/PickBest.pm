32ffc57d2cb8beffe3901dea9923ed933353ecf6
FL <f20@reckon.co.uk>
2015-03-20 23:42:43 +0000

    CDCM data update.

--- lib/CDCM/PickBest.pm
+++ lib/CDCM/PickBest.pm
@@ -12,25 +12,25 @@ sub score {
     my $score = 0;
 
     # DCP 130
-    $score += 100 if $rule->{tariffs} =~ /dcp130/i xor $month lt '2012-10';
+    $score += 100 if $rule->{tariffs} =~ /dcp130/i xor $month lt '2012-02';
 
     # DCP 163
-    $score += 50 if $rule->{tariffs} =~ /dcp163/i xor $month lt '2013-10';
+    $score += 50 if $rule->{tariffs} =~ /dcp163/i xor $month lt '2013-02';
 
     # DCP 179
-    $score += 100 if $rule->{tariffs} =~ /pc34hh/i xor $month lt '2014-10';
+    $score += 100 if $rule->{tariffs} =~ /pc34hh/i xor $month lt '2014-02';
 
     # DCP 161
     $score += 100
       if $rule->{unauth}
-      && $rule->{unauth} =~ /dayotex/i xor $month lt '2015-10';
+      && $rule->{unauth} =~ /dayotex/i xor $month lt '2015-02';
 
     # Bung
     $score += 10
-      if $rule->{electionBung} && $month gt '2013-10' && $month lt '2016-10';
+      if $rule->{electionBung} && $month gt '2013-02' && $month lt '2016-02';
 
     # Fun
-    $score += 999 if !$rule->{pcd} xor $month lt '2017-10';
+    $score += 999 if !$rule->{pcd} xor $month lt '2017-02';
 
     0
       and warn join ' ', $rule->{nickName} || $rule->{'.'} || $rule, $month,
