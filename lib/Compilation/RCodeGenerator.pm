d1104102422b6cb7b4f434230ab78e882ba5ad8c
FL <f20@reckon.co.uk>
2015-09-10 18:49:45 +0000

    Refactoring and minor fixes, primarily of the poor collection of tests.

--- lib/Compilation/RCodeGenerator.pm
+++ lib/Compilation/RCodeGenerator.pm
@@ -2,8 +2,17 @@
 
 =head Copyright licence and disclaimer
 
-Copyright 2014-2015 Reckon LLP. All rights reserved.
-Please do not use or distribute without permission.
+Copyright 2014-2015 Franck Latrémolière, Reckon LLP and others.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
 
 THIS SOFTWARE IS PROVIDED BY AUTHORS AND CONTRIBUTORS "AS IS" AND ANY
 EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
