2bebf83df2d6a5c774efbe6fc5d520e30b9c92d1
FL <f20@reckon.co.uk>
2016-10-05 08:46:10 +0000

    VBA tools: minor improvements.

--- VBA/PasswordBruteForce.vb
+++ VBA/PasswordBruteForce.vb
@@ -1,6 +1,6 @@
-Function GetAPassword(sheet As Worksheet) As String
+Function GuessSheetPassword(sheet As Worksheet) As String
   ' Author unknown but submitted by brettdj of www.experts-exchange.com
-  ' Modified by Franck Latrémolière
+  ' Adapted by Franck Latremoliere
   Dim i As Integer, j As Integer, k As Integer
   Dim l As Integer, m As Integer, n As Integer
   Dim i1 As Integer, i2 As Integer, i3 As Integer
@@ -14,13 +14,35 @@ Function GetAPassword(sheet As Worksheet) As String
         Chr(l) & Chr(m) & Chr(i1) & Chr(i2) & Chr(i3) & _
         Chr(i4) & Chr(i5) & Chr(i6) & Chr(n)
     If sheet.ProtectContents = False Then
-        GetAPassword = Chr(i) & Chr(j) & Chr(k) & Chr(l) & Chr(m) & Chr(i1) & _
+        GuessSheetPassword = Chr(i) & Chr(j) & Chr(k) & Chr(l) & Chr(m) & Chr(i1) & _
             Chr(i2) & Chr(i3) & Chr(i4) & Chr(i5) & Chr(i6) & Chr(n)
         Exit Function
     End If
   Next: Next: Next: Next: Next: Next
   Next: Next: Next: Next: Next: Next
 End Function
+
+Sub GuessBookPassword(book As Workbook)
+  ' Author unknown but submitted by brettdj of www.experts-exchange.com
+  ' Adapted by Franck Latremoliere
+  Dim i As Integer, j As Integer, k As Integer
+  Dim l As Integer, m As Integer, n As Integer
+  Dim i1 As Integer, i2 As Integer, i3 As Integer
+  Dim i4 As Integer, i5 As Integer, i6 As Integer
+  On Error Resume Next
+  For i = 65 To 66: For j = 65 To 66: For k = 65 To 66
+  For l = 65 To 66: For m = 65 To 66: For i1 = 65 To 66
+  For i2 = 65 To 66: For i3 = 65 To 66: For i4 = 65 To 66
+  For i5 = 65 To 66: For i6 = 65 To 66: For n = 32 To 126
+    book.Unprotect Chr(i) & Chr(j) & Chr(k) & _
+        Chr(l) & Chr(m) & Chr(i1) & Chr(i2) & Chr(i3) & _
+        Chr(i4) & Chr(i5) & Chr(i6) & Chr(n)
+    If book.ProtectStructure = False Then
+        Exit Sub
+    End If
+  Next: Next: Next: Next: Next: Next
+  Next: Next: Next: Next: Next: Next
+End Sub
 
 Public Sub PasswordBreakerAllSheets()
     Set sl = ActiveWorkbook.Worksheets
@@ -29,13 +51,23 @@ Public Sub PasswordBreakerAllSheets()
         Dim sheet As Worksheet
         Set sheet = sl(sn2)
         If sheet.ProtectContents = True Then
-            pass = GetAPassword(sheet)
+            pass = GuessSheetPassword(sheet)
             For sn = 1 To slc
                 If sl(sn).ProtectContents = True Then
                     sl(sn).Unprotect pass
                 End If
+                If ActiveWorkbook.ProtectStructure = True Then
+                    ActiveWorkbook.Unprotect pass
+                End If
             Next sn
         End If
     Next sn2
+    If ActiveWorkbook.ProtectStructure = True Then
+        Call GuessBookPassword(ActiveWorkbook)
+    End If
 End Sub
+
+
+
+
 
