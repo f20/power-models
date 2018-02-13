2a5e3c0d15b0ab53e66e8c7848fdab1fc43c08b5
FL <f20@reckon.co.uk>
2018-02-13 09:10:37 +0000

    Code cosmetics.

--- pmod.pl
+++ pmod.pl
@@ -2,7 +2,7 @@
 
 =head Copyright licence and disclaimer
 
-Copyright 2011-2017 Franck Latrémolière and others. All rights reserved.
+Copyright 2011-2018 Franck Latrémolière and others. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -29,7 +29,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 use warnings;
 use strict;
-use utf8;
 binmode STDIN,  ':utf8';
 binmode STDOUT, ':utf8';
 binmode STDERR, ':utf8';
@@ -37,7 +36,7 @@ use File::Spec::Functions qw(rel2abs catdir);
 use File::Basename qw(dirname);
 use Cwd qw(getcwd);
 
-my ( @homes, @validatedLibs, @otherLibs );
+my ( @homes, @libsSubjectToCodeValidation, @libsWithoutValidation );
 
 BEGIN {
     my @paths      = ( getcwd() );
@@ -54,9 +53,9 @@ BEGIN {
             push @homes, $folder if -e catdir( $folder, 'models' );
             my $lib = catdir( $folder, 'lib' );
             if ( -d $lib ) {
-                push @validatedLibs, $lib;
+                push @libsSubjectToCodeValidation, $lib;
                 $lib = catdir( $folder, 'cpan' );
-                push @otherLibs, $lib if -d $lib;
+                push @libsWithoutValidation, $lib if -d $lib;
                 last;
             }
             my $parent = dirname $folder;
@@ -65,13 +64,15 @@ BEGIN {
         }
     }
 }
-use lib @validatedLibs, @otherLibs;
+use lib @libsWithoutValidation, @libsSubjectToCodeValidation;
 
 use PowerModels::CLI::CommandParser;
 my $parser = PowerModels::CLI::CommandParser->new;
 @ARGV ? $parser->acceptCommand(@ARGV) : $parser->acceptScript( \*STDIN );
 
 use PowerModels::CLI::CommandRunner;
-my $runner = PowerModels::CLI::CommandRunner->new( \@homes, \@validatedLibs );
+my $runner =
+  PowerModels::CLI::CommandRunner->new( \@homes,
+    \@libsSubjectToCodeValidation );
 $parser->run($runner);
 $runner->finish;
